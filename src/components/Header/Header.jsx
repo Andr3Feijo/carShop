22f9f47d00c3a3ba3edc75637db656de51d03f80
André Feijó <107944135+Alsf30@users.noreply.github.com>
2023-06-15 22:14:57 -0300

    iniciando input search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 function Header() {
-  
+  return(
+    <header className="header" style={{backgroundColor: 'red'}} >
+      <div className="containner">
+        <form action="">
+          <input 
+          type="search" 
+          placeholder="Buscar Produto"
+          className="search__input"
+          required
+          />
+        </form>
+      </div>
+
+
+
+    </header>
+  );
 }
 
 
